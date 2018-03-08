2eb789d1ac0bb0dc1ce88bf12595468de5ae8e38
temma.io <eifan.devs@gmail.com>
2018-03-08 21:54:28 +0900

    ルーティング修正

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index() {
-        $path = storage_path() . "/index.html";
+        $path = "/index.html";
         $client_html = file_get_contents($path, true);
         return $client_html;
     }
