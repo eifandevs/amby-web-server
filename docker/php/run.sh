09aec47d944dfa3bf8ca8a811f461c072c562495
temma.io <eifan.devs@gmail.com>
2018-04-30 03:06:42 +0900

    run.sh修正

--- docker/php/run.sh
+++ docker/php/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# server git clone(shallow clone)
-git clone --depth=1 https://github.com/eifandevs/qas-news-server.git
-
 # client git clone(shallow clone)
 git clone --depth=1 https://github.com/eifandevs/qas-news-client.git
+
+# server setting
+pushd qas-news-server
+
+git init
+git config --global user.email "eifan.devs@gmail.com"
+git config --global user.name "temma.io"
+git add .
+git commit -m "initial commit"
+git remote add origin https://github.com/eifandevs/qas-news-server.git
+git fetch origin --depth 1
+git rebase origin/master
+
+popd
