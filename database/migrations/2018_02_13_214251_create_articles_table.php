5075095676b47b619815d5e7a4851e31a2cb7878
temma.io <eifan.devs@gmail.com>
2018-05-05 07:27:47 +0900

    articlesテーブル情報修正

--- database/migrations/2018_02_13_214251_create_articles_table.php
+++ database/migrations/2018_02_13_214251_create_articles_table.php
@@ -22,7 +22,8 @@ class CreateArticlesTable extends Migration
             $table->string('url');
             $table->string('image_url');
             $table->string('published_time');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
