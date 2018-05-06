d16f64f5bb4537ae450cfbb93027c933bbaaa965
temma.io <eifan.devs@gmail.com>
2018-05-06 14:29:12 +0900

    storageフォルダの権限修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -11,21 +11,21 @@ class ArticleController extends Controller
         \Log::debug('article get.');
 
         // ダミーデータ作成
-        $article = new Article;
-        $article->source_name = "test";
-        $article->author = "test";
-        $article->title = "test";
-        $article->description = "test";
-        $article->url = "test";
-        $article->image_url = "test";
-        $article->published_time = "test";
-        $article->save();
+        // $article = new Article;
+        // $article->source_name = "test";
+        // $article->author = "test";
+        // $article->title = "test";
+        // $article->description = "test";
+        // $article->url = "test";
+        // $article->image_url = "test";
+        // $article->published_time = "test";
+        // $article->save();
 
         // 検索
-        $articles = Article::all();
-        foreach ($articles as $row) {
-            \Log::debug($row->url);
-        }
+        // $articles = Article::all();
+        // foreach ($articles as $row) {
+        //     \Log::debug($row->url);
+        // }
 
         $path = storage_path() . "/mock/article_data.json";
         $aricle_data = json_decode(file_get_contents($path), true);
