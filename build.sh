ff75475dcc1d07bc6af6caf379c654a5ef7ff131
temma.io <eifan.devs@gmail.com>
2018-05-13 02:32:48 +0900

    ビルドシェルをプルシェルに分けた

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# ソースを最新にする
-git fetch origin
-git rebase origin/master
-
 # ビルド
-RUN composer install
+composer install
 npm install
---
+++ pull.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# ソースを最新にする
+git fetch origin
+git rebase origin/master
