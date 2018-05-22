69c4ed13627c53f0ef09c82b9435b27a05e49ca6
temma.io <eifan.devs@gmail.com>
2018-05-22 22:35:03 +0900

    create migration container

--- docker/migration/entrypoint.sh
+++ docker/migration/entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-cd /var/www/qass-news-server
-
 # dbコンテナの起動を待つ
 until mysqladmin ping -hdb --silent; do
     echo 'waiting for mysqld to be connectable...'
     sleep 3
 done
 
-php artisan migrate
-
-while true
-do
-    sleep 10
-done
+curl nginx/api/migrate -X PUT
