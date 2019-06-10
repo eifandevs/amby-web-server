523f8865832bc5d5052fb3ae32dcf134bb6df8ee
temma.io <eifan.devs@gmail.com>
2019-06-10 23:27:53 +0900

    add report table

--- database/migrations/2019_06_10_142731_create_reports_table.php
+++ database/migrations/2019_06_10_142731_create_reports_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateReportsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reports', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string("login_id")->unique();
-            $table->string("login_password");
-            $table->string("email")->unique();
-            $table->string("access_token");
-            $table->date("access_token_expire_date");
+            $table->string("report_id")->unique();
+            $table->string("messages")->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reports');
     }
 }
