0aa62f365cdada171fb00f5d863ae17de0cb863b
temma.io <eifan.devs@gmail.com>
2019-08-25 21:45:44 +0900

    php -> go

--- docker/migration/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# dbコンテナの起動を待つ
-until mysqladmin ping -hdb --silent; do
-    echo 'waiting for mysqld to be connectable...'
-    sleep 3
-done
-
-curl -k https://nginx/api/migrate -X PUT
