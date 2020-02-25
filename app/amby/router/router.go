d30d1ddd8de1b17b2e859bf79750d6385cb06fdf
temma.io <eifan.devs@gmail.com>
2020-02-26 08:38:10 +0900

    update

--- app/amby/router/router.go
+++ app/amby/router/router.go
@@ -1,33 +1,41 @@
 package router
 
 import (
-  "fmt"
-  "github.com/eifandevs/amby/controllers"
-  "github.com/eifandevs/amby/interceptor"
-  "github.com/labstack/echo"
-  "github.com/labstack/echo/middleware"
+	"fmt"
+
+	"github.com/eifandevs/amby/controllers"
+	"github.com/eifandevs/amby/interceptor"
+	"github.com/eifandevs/amby/mock"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func bodyDumpHandler(c echo.Context, reqBody, resBody []byte) {
-  fmt.Printf("Request Body: %v\n", string(reqBody))
-  fmt.Printf("Response Body: %v\n", string(resBody))
+	fmt.Printf("Request Body: %v\n", string(reqBody))
+	fmt.Printf("Response Body: %v\n", string(resBody))
 }
 
 func Init() *echo.Echo {
-	
-  e := echo.New()
 
-  e.Use(middleware.BodyDump(bodyDumpHandler))
+	e := echo.New()
 
-  api := e.Group("/api")
-  {
-    api.GET("/accesstoken", controllers.GetAccessToken(), interceptor.BasicAuth())
-    api.GET("/favorite", controllers.GetHandler())
-    api.POST("/favorite", controllers.PostHandler())
-  }
+	e.Use(middleware.BodyDump(bodyDumpHandler))
 
-  e.Static("/static", "./static")
-  e.GET("/digest", interceptor.DigestAuthenticate())
+	api := e.Group("/api")
+	{
+		api.GET("/accesstoken", controllers.GetAccessToken(), interceptor.BasicAuth())
+		api.GET("/favorite", controllers.GetHandler())
+		api.POST("/favorite", controllers.PostHandler())
+	}
 
-  return e
+	mockApi := e.Group("/mock")
+	{
+		mockApi.GET("/test", mock.GetTest())
+		mockApi.POST("/test", mock.PostTest())
+	}
+
+	e.Static("/static", "./static")
+	e.GET("/digest", interceptor.DigestAuthenticate())
+
+	return e
 }
