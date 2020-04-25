d56f7dbbe1f2709b91cbe93038f90c29771b69b2
temma.io <eifan.devs@gmail.com>
2020-04-26 00:42:12 +0900

    update

--- app/amby/cmd/migrate/create_accesstoken.go
+++ app/amby/cmd/migrate/create_accesstoken.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"github.com/eifandevs/amby/models"
-	"github.com/eifandevs/amby/repo"
+  "github.com/eifandevs/amby/repo"
+  "math/rand"
+  "time"
 )
 
 func main() {
   db := repo.Connect("development")
   defer db.Close()
 
-  // create accesstoken table
-  db.Create(&models.AccessToken{Token: "token", Expire: "2019-10-10T13:50:40+09:00"})
+  token := createToken(20)
+  expireDate := createExpireDate()
+
+  db.Create(&models.AccessToken{Token: token, Expire: expireDate})
+}
+
+func createToken(n int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
+
+func createExpireDate() string {
+  jst, _ := time.LoadLocation("Asia/Tokyo")
+  now := time.Now()
+  // 現在+90日
+  return time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, jst).Add(24 * time.Hour * 90).String()
 }
